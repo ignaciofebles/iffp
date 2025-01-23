f5e1be613c2eec84768a13f53184973f99e7428e
ignaciofebles <wintotum@gmail.com>
2025-01-23 02:34:23 +0100

    Traslado de datos

--- thing/views.py
+++ thing/views.py
@@ -3,6 +3,7 @@ from thing.models import Thing
 from .forms import ThingForm
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Q
 
 # Create your views here.
 def thing(request):
@@ -19,23 +20,39 @@ def thing(request):
 
 
 
+# def things_list(request):
+#     search_term = request.GET.get('search', '').strip()
+
+#     if search_term:
+#         things = Thing.objects.filter(usuario=request.user, code__icontains=search_term).order_by('code')
+
+#         if not things.exists():
+#             things = Thing.objects.filter(usuario=request.user, description__icontains=search_term).order_by('code')
+#     else:
+#         things = Thing.objects.filter(usuario=request.user).order_by('code')
+    
+        
+#     context = {
+#         'things': things,
+#     }
+#     return render(request, 'things_list.html', context)
+
 def things_list(request):
     search_term = request.GET.get('search', '').strip()
 
     if search_term:
-        things = Thing.objects.filter(usuario=request.user, code__icontains=search_term).order_by('code')
-
-        if not things.exists():
-            things = Thing.objects.filter(usuario=request.user, description__icontains=search_term).order_by('code')
+        # Usar Q para buscar en 'code' y 'description'
+        things = Thing.objects.filter(
+            Q(usuario=request.user) & 
+            (Q(code__icontains=search_term) | Q(description__icontains=search_term))
+        ).order_by('code')
     else:
         things = Thing.objects.filter(usuario=request.user).order_by('code')
-    
-        
+
     context = {
         'things': things,
     }
     return render(request, 'things_list.html', context)
-
 
 
 class ThingEditView(UpdateView):
@@ -49,3 +66,7 @@ class ThingDeleteView(DeleteView):
     model = Thing
     template_name = 'thing_confirm_delete.html'
     success_url = reverse_lazy('things_list') 
+
+
+    from django.db.models import Q
+
